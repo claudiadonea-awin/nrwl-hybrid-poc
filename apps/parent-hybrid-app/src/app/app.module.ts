dca52d0b6061af1215d81088319fd50072f8299c
claudia donea <claudia.donea@awin.com>
2018-01-05 14:23:20 +0000

    added project to nrwl setup project

--- apps/parent-hybrid-app/src/app/app.module.ts
+++ apps/parent-hybrid-app/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
+import { configureNwrlPoc, upgradedComponents } from '../nwrl-poc-setup';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 @NgModule({
-  imports: [BrowserModule, NxModule.forRoot()],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, NxModule.forRoot(), UpgradeModule],
+  declarations: [AppComponent, ...upgradedComponents],
+  entryComponents: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+
+  ngDoBootstrap(): void {
+    configureNwrlPoc(this.upgrade.injector);
+    this.upgrade.bootstrap(document.body, ['downgraded', 'nwrl-poc']);
+  }
+}
